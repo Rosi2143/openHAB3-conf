8791658e6f28488a118779fbac27f9799c7cbe43
Rosi2143 <Schrott.Micha@web.de>
2022-12-03 21:19:41 +0100

    cleanup test scripts
    Signed-off-by: Rosi2143 <Schrott.Micha@web.de>

--- automation/jsr223/python/personal/hello_world.py
+++ automation/jsr223/python/personal/hello_world.py
@@ -1,11 +1,20 @@
 """
-This script uses a decorated cron rule that will generate logs every 10s and can be used to test your initial setup.
+This script uses a decorated cron rule that will generate logs every 10s
+and can be used to test your initial setup.
 """
 
 from core.rules import rule
 from core.triggers import when
 
-@rule("Jython Hello World (cron decorators)", description="This is an example cron triggered rule using decorators", tags=["Test tag", "Hello World"])# description and tags are optional
+
+# description and tags are optional
+@rule("Jython Hello World (cron decorators)", description="This is an example cron triggered rule using decorators", tags=["Test tag", "Hello World"])
 @when("Time cron 0/10 * * * * ?")
 def hello_world_cron_decorators(event):
+    """
+    prints Hello World
+
+    Args:
+        event (_type_): _description_
+    """
     hello_world_cron_decorators.log.info("Hello World!")
