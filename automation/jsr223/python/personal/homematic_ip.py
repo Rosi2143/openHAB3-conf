97fe07d21151f0ae6bf5126cf6b41c4d1e1e600c
Rosi2143 <Schrott.Micha@web.de>
2023-03-01 20:16:37 +0100

    add rule for overheat protection
    Signed-off-by: Rosi2143 <Schrott.Micha@web.de>

--- automation/jsr223/python/personal/homematic_ip.py
+++ automation/jsr223/python/personal/homematic_ip.py
@@ -32,7 +32,7 @@ def homematic_ip_shed_door(event):
       description="handle activation of door bell",
       tags=["channeltriggered", "HomematicIP", "doorbell"])
 @when("Channel homematic:HmIP-DSD-PCB:homematicBridge-90f3312b69:0026DBE998F796:1#BUTTON triggered SHORT_PRESS")
-def homematic_ip_ring(event):
+def homematic_ip_ring_start(event):
     """
     handle press on door bell
 
@@ -40,7 +40,7 @@ def homematic_ip_ring(event):
         event (_type_): triggering event
     """
 
-    homematic_ip_ring.log.info(
+    homematic_ip_ring_start.log.info(
         "rule fired")
 
     events.sendCommand("HabPanelDashboardName", "CameraSnap")
@@ -53,7 +53,7 @@ def homematic_ip_ring(event):
       description="handle deactivation of door bell",
       tags=["itemchange", "HomematicIP", "doorbell"])
 @when("Item KlingelerkennungFlur_StoreState changed to OFF")
-def homematic_ip_ring(event):
+def homematic_ip_ring_end(event):
     """
     door bell status was reset
 
@@ -61,7 +61,59 @@ def homematic_ip_ring(event):
         event (_type_): triggering event
     """
 
-    homematic_ip_ring.log.info(
+    homematic_ip_ring_end.log.info(
         "rule fired")
 
     events.sendCommand("HabPanelDashboardName", "Erdgeschoss")
+
+MAX_ALLOWED_TEMP = 27
+NEW_ALLOWED_TEMP = 20
+OVERHEAT_STRING = "_HitzeSchutzTimer"
+
+@rule("HomematicIP: OverHeatProtectionStart",
+      description="start timer to prevent too high temperatures",
+      tags=["memberchange", "HomematicIP", "overheat"])
+@when("Member of gThermostate_SetPointModes changed")
+def homematic_overheat_protection_start(event):
+    """
+    make sure the temperature is not too high for too long
+
+    Args:
+        event (_type_): triggering event
+    """
+
+    homematic_overheat_protection_start.log.info(
+        "rule fired because of %s %s --> %s", event.itemName, event.oldItemState, event.itemState)
+
+    set_temp = event.itemState.intValue()
+
+    if set_temp >= MAX_ALLOWED_TEMP:
+        timer_item_name = event.itemName + OVERHEAT_STRING
+        timer_item = itemRegistry.getItems(timer_item_name)
+        if timer_item == []:
+            homematic_overheat_protection_start.log.info("TimerItem: " + timer_item.name + " does not exist.")
+        else:
+            events.sendCommand(timer_item, "ON")
+
+@rule("HomematicIP: OverHeatProtectionEnd",
+      description="end of timer to prevent too high temperatures",
+      tags=["memberchange", "HomematicIP", "overheat"])
+@when("Member of gThermostate_HitzeSchutzTimer changed to OFF")
+def homematic_overheat_protection_end(event):
+    """
+    set temperature back to "normal" after the timeout
+
+    Args:
+        event (_type_): triggering event
+    """
+
+    homematic_overheat_protection_end.log.info(
+        "rule fired because of %s %s --> %s", event.itemName, event.oldItemState, event.itemState)
+
+    item_name = event.itemName.replace(OVERHEAT_STRING, "")
+    set_temp = items[item_name].intValue()
+    new_temp = str(NEW_ALLOWED_TEMP)
+
+    if set_temp >= MAX_ALLOWED_TEMP:
+        homematic_overheat_protection_end.log.info("Reset: " + item_name + " to lower temperature of " + new_temp + "°C.")
+        events.sendCommand(item_name, new_temp)
